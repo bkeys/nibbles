521aacf9a22054f339b7a5c506be25e12748b749
ComradeKeys <key13005@byui.edu>
2015-10-07 23:42:42 -0500

    Added rotation for pots

--- src/Obstacle.hpp
+++ src/Obstacle.hpp
@@ -21,13 +21,20 @@
 class Item : public Element {
 
 public:
+  Item() {rotation = rand() % 360;};
   void draw() {
     glColor3ub(0, 255, 0);
     glPushMatrix();
     glTranslatef(x, y, -y);
+    glRotatef(rotation, 0, 1, 0);
     glutSolidTeapot(.7);
     glPopMatrix();
   }
+
+private:
+  float rotation;
+
+
 };
 
 class Obstacle : public Element {
